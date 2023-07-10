69fc8a9d6f878fcd7421588ccf1d25fe649f6c51
Your Name <you@example.com>
2023-07-10 18:30:22 +0800

    add log

---
+++ code/log/log.cpp
@@ -0,0 +1,591 @@
+#include "log.h"
+
+//LogLevel
+namespace hxk
+{
+
+std::string LogLevel::levelToString(LogLevel::Level level)
+{
+    switch (level)
+    {
+    case UNKNOWN:
+        return "UNKONWN";
+    case DEBUG:
+        return "DEBUG";
+    case INFO:
+        return "INFO ";
+    case WARN:
+        return "WARN ";
+    case ERROR:
+        return "ERROR";
+    case FATAL:
+        return "FATAL";
+    default:
+        return "UNKONWN";
+        break;
+    }
+}
+
+}
+
+//LogEvent
+namespace hxk
+{
+
+LogEvent::LogEvent(const std::string& filename, uint32_t line, uint32_t threadid, uint32_t fiberid, 
+                    time_t time, const std::string& content, LogLevel::Level level)
+        :m_level(level), m_filename(filename), m_line(line), 
+        m_thread_id(threadid), m_fiber_id(fiberid), m_time(time),
+        m_content(content){
+
+        }
+
+LogLevel::Level LogEvent::getLevel() const 
+{ 
+    return m_level;
+}
+
+void LogEvent::setLevel(LogLevel::Level level) 
+{
+    m_level = level;
+}
+
+const std::string& LogEvent::getFilename() const 
+{ 
+    return m_filename;
+}
+
+uint32_t LogEvent::getLine() const 
+{
+    return m_line;
+}
+
+uint32_t LogEvent::getThreadId() const 
+{ 
+    return m_thread_id;
+}
+
+uint32_t LogEvent::getFiberId() const 
+{ 
+    return m_fiber_id; 
+}
+
+time_t LogEvent::getTime() const 
+{ 
+    return m_time; 
+}
+
+const std::string& LogEvent::getContent() const 
+{ 
+    return m_content;
+}
+}
+/**
+ * FormatItem
+ * %p 输出日志等级
+ * %f 输出文件名
+ * %l 输出行号
+ * %d 输出日志时间
+ * %t 输出线程号
+ * %F 输出协程号
+ * %m 输出日志消息
+ * %n 输出换行
+ * %% 输出百分号
+ * %T 输出制表符
+ * */
+namespace hxk
+{
+
+/**
+ * @Author: hxk
+ * @brief: 输出字符串
+ * @return {*}
+ */
+class PlainFormatItem : public FormatItem
+{
+public:
+    explicit PlainFormatItem(const std::string& str) : m_str(str){}
+    void format(std::ostream& out, LogEvent::_ptr ev) override
+    {
+        out << m_str;
+    }
+private:
+    std::string m_str;
+};
+
+/**
+ * @Author: hxk
+ * @brief: 输出日志等级
+ * @return {*}
+ */
+class LevelFormatItem : public FormatItem
+{
+public: 
+    void format(std::ostream& out, LogEvent::_ptr ev)
+    {
+        out << LogLevel::levelToString(ev->getLevel());
+    }
+};
+
+/**
+ * @Author: hxk
+ * @brief: 输出文件名
+ * @return {*}
+ */
+class FileNameFormatItem : public FormatItem
+{
+public: 
+    void format(std::ostream& out, LogEvent::_ptr ev)
+    {
+        out << ev->getFilename();
+    }
+};
+
+/**
+ * @Author: hxk
+ * @brief: 输出行号
+ * @return {*}
+ */
+class LineFormatItem : public FormatItem
+{
+public: 
+    void format(std::ostream& out, LogEvent::_ptr ev)
+    {
+        out << ev->getLine();
+    }
+};
+
+/**
+ * @Author: hxk
+ * @brief: 输出线程id
+ * @return {*}
+ */
+class ThreadIdFormatItem : public FormatItem
+{
+public: 
+    void format(std::ostream& out, LogEvent::_ptr ev)
+    {
+        out << ev->getThreadId();
+    }
+};
+
+/**
+ * @Author: hxk
+ * @brief: 输出协程id
+ * @return {*}
+ */
+class FiberIdFormatItem : public FormatItem
+{
+public: 
+    void format(std::ostream& out, LogEvent::_ptr ev)
+    {
+        out << ev->getFiberId();
+    }
+};
+
+/**
+ * @Author: hxk
+ * @brief: 输出时间
+ * @return {*}
+ */
+class TimeFormatItem : public FormatItem
+{
+public: 
+    explicit TimeFormatItem(const std::string &str = "%Y-%m-%d %H:%M:%S"):m_time_pattern(str)
+    {
+        if(m_time_pattern.empty()){
+            m_time_pattern = "%Y-%m-%d %H:%M:%S";
+        }
+    }
+    void format(std::ostream& out, LogEvent::_ptr ev)
+    {
+        struct tm t;
+        time_t tt = ev->getTime();
+        localtime_r(&tt,&t);
+        char time_buf[64]={0};
+        strftime(time_buf, sizeof(time_buf), m_time_pattern.c_str(),&t);
+        out << time_buf;
+    }
+private:
+    std::string m_time_pattern;
+};
+
+/**
+ * @Author: hxk
+ * @brief: 输出内容
+ * @return {*}
+ */
+class ContentFormatItem : public FormatItem
+{
+public: 
+    void format(std::ostream& out, LogEvent::_ptr ev)
+    {
+        out << ev->getContent();
+    }
+};
+
+/**
+ * @Author: hxk
+ * @brief: 空行
+ * @return {*}
+ */
+class NewLineFormatItem : public FormatItem
+{
+public: 
+    void format(std::ostream& out, LogEvent::_ptr ev)
+    {
+        out << std::endl;
+    }
+};
+
+/**
+ * @Author: hxk
+ * @brief: 百分号 %
+ * @return {*}
+ */
+class PercentSignFormatItem : public FormatItem
+{
+public: 
+    void format(std::ostream& out, LogEvent::_ptr ev)
+    {
+        out << "%";
+    }
+};
+
+/**
+ * @Author: hxk
+ * @brief: Tab制表符
+ * @return {*}
+ */
+class TabFormatItem : public FormatItem
+{
+public: 
+    void format(std::ostream& out, LogEvent::_ptr ev)
+    {
+        out << '\t';
+    }
+};
+
+
+/*
+有且只有 thread_local 关键字修饰的变量具有线程（thread）周期，
+这些变量在线程开始的时候被生成，在线程结束的时候被销毁，
+并且每一个线程都拥有一个独立的变量实例。
+*/                                 
+thread_local static std::map<char, FormatItem::_ptr> format_item_map{
+#define FN(CH, ITEM_NAME)                   \
+    {                                       \
+        CH, std::make_shared<ITEM_NAME>()   \
+    } 
+    FN('p', LevelFormatItem),
+    FN('f', FileNameFormatItem),
+    FN('l', LineFormatItem),
+    FN('d', TimeFormatItem),
+    FN('F', FiberIdFormatItem),
+    FN('t', ThreadIdFormatItem),
+    FN('m', ContentFormatItem),
+    FN('n', NewLineFormatItem),
+    FN('%', PercentSignFormatItem),
+    FN('T', TabFormatItem),
+#undef FN
+};
+}
+
+//LogFormatter
+namespace hxk
+{
+LogFormatter::LogFormatter(const std::string& pattern):m_format_pattern(pattern)
+{
+    init();
+}
+
+std::string LogFormatter::format(LogEvent::_ptr ev)
+{
+    std::stringstream ss;
+    for(auto& item : m_formatItem_list){
+        item->format(ss, ev);
+    }
+    return ss.str();
+}
+
+void LogFormatter::init()
+{
+    enum PARSE_STATUS{
+        SCAN_STATUS,    //扫描到普通字符
+        CREATE_STATUS   //扫描到%，处理占位字符
+    };
+    PARSE_STATUS status = SCAN_STATUS;
+    size_t str_begin = 0;
+    size_t str_end = 0;
+    //[%d] [%p] [T:%t F:%F]%T%m%n
+    for(size_t i=0; i<m_format_pattern.length();i++){
+        switch(status)
+        {
+            case SCAN_STATUS:
+                str_begin = i;
+                for(str_end = i; str_end < m_format_pattern.length();str_end++){
+                    if(m_format_pattern[str_end] == '%'){
+                        status = CREATE_STATUS;
+                        break;
+                    }
+                }
+                i=str_end;
+                m_formatItem_list.push_back(std::make_shared<PlainFormatItem>(
+                    m_format_pattern.substr(str_begin, str_end-str_begin)));
+                break;
+            case CREATE_STATUS:
+                assert(!format_item_map.empty() && "format_item_map don't init correctly");
+                auto iter = format_item_map.find(m_format_pattern[i]);  //因为上一个case会让i加1
+                if(iter == format_item_map.end()){
+                    m_formatItem_list.push_back(std::make_shared<PlainFormatItem>("<error fromat>"));
+                }
+                else{
+                    m_formatItem_list.push_back(iter->second);
+                }
+                status = SCAN_STATUS;
+                break;
+        }
+    }
+}
+}
+
+//LogAppender && StdoutLogAppender && FileLogAppender
+namespace hxk
+{
+//LogAppender
+LogAppender::LogAppender(LogLevel::Level level):m_level(level)
+{
+
+}
+
+LogFormatter::_ptr LogAppender::getFormatter()
+{
+    ScopedLock lock(&m_mutex);
+    return m_formatter;
+}
+
+void LogAppender::setFormatter(LogFormatter::_ptr formatter)
+{
+    ScopedLock lock(&m_mutex);
+    m_formatter = std::move(formatter);
+}
+
+//StdoutLogAppender
+StdoutLogAppender::StdoutLogAppender(LogLevel::Level level) : LogAppender(level)
+{
+
+}
+
+void StdoutLogAppender::log(LogLevel::Level level, LogEvent::_ptr ev)
+{
+    if(level < m_level)
+    {
+        return;
+    }
+    ScopedLock lock(&m_mutex);
+    std::cout << m_formatter->format(ev);
+    std::cout.flush();
+}
+
+//FileLogAppender
+FileLogAppender::FileLogAppender(const std::string& filename, LogLevel::Level level):LogAppender(level), m_filename(filename)
+{
+    reopen();
+}
+
+bool FileLogAppender::reopen()
+{
+    ScopedLock lock(&m_mutex);
+    /*
+    使用 fstream::operator!() 操作符来检查文件流对象 m_file_stream 是否有效
+    true if an error has occurred, false otherwise.
+    */
+    if(!m_file_stream){     
+        m_file_stream.close();
+    }
+    m_file_stream.open(m_filename, std::ios_base::out | std::ios_base::app);
+    return !!m_file_stream;
+}
+
+void FileLogAppender::log(LogLevel::Level level, LogEvent::_ptr ev)
+{
+    if(level < m_level){
+        return ;
+    }
+    ScopedLock lock(&m_mutex);
+    m_file_stream << m_formatter->format(ev);
+    m_file_stream.flush();
+}
+}
+
+//Logger
+namespace hxk
+{
+Logger::Logger():m_name("default"), m_level(LogLevel::DEBUG), 
+                    m_formatter_pattern("[%d] [%p] [T:%t F:%F]%T%m%n")
+{
+    m_formatter.reset(new LogFormatter(m_formatter_pattern));
+}
+
+Logger::Logger(const std::string& name, LogLevel::Level level, const std::string& pattern)
+                :m_name(name), m_level(level), m_formatter_pattern(pattern)
+{
+    m_formatter.reset(new LogFormatter(m_formatter_pattern));
+}
+
+void Logger::addAppender(LogAppender::_ptr appender)
+{
+    ScopedLock lock(&m_mutex);
+    if(!appender->getFormatter()){
+        appender->setFormatter(m_formatter);
+    }
+    m_appender_list.push_back(appender);
+}
+
+void Logger::delAppender(LogAppender::_ptr appender)
+{
+    ScopedLock lock(&m_mutex);
+    auto iter = std::find(m_appender_list.begin(), m_appender_list.end(), appender);
+    if(iter != m_appender_list.end()){
+        m_appender_list.erase(iter);
+    }
+}
+
+LogLevel::Level Logger::getLevel() const 
+{ 
+    return m_level;
+}
+
+void Logger::setLevel(LogLevel::Level level) 
+{ 
+    m_level = level;
+}
+
+std::string Logger::getName() const 
+{ 
+    return m_name; 
+}
+
+void Logger::log(LogEvent::_ptr ev)
+{
+    if(ev->getLevel() < m_level){
+        return;
+    }
+    ScopedLock lock(&m_mutex);
+    for(auto& iter:m_appender_list){
+        iter->log(ev->getLevel(), ev);
+    }
+}
+
+}
+
+//__LoggerManager
+namespace hxk
+{
+__LoggerManager::__LoggerManager()
+{
+    init();
+}
+
+void __LoggerManager::init(std::vector<LogConfig> config_log_list)
+{
+    ScopedLock lock(&m_mutex);
+    auto config = Config::lookUp<std::vector<LogConfig>>("logs");
+    
+    //const auto& config_log_list = config->getValue();
+    // std::cout << "init" << config_log_list.size() << std::endl;
+    for(const auto& config_log : config_log_list) {
+        m_logger_map.erase(config_log.name);    //删除同名的logger
+        auto logger = std::make_shared<Logger>(config_log.name,config_log.level,config_log.formatter);
+
+        for(const auto& config_appender : config_log.appender) {
+            LogAppender::_ptr appender;
+            switch (config_appender.output_type)
+            {
+                case LogAppenderConfig::STDOUT:
+                    appender = std::make_shared<StdoutLogAppender>(config_appender.level);
+                    break;
+                case LogAppenderConfig::FILE:
+                    appender = std::make_shared<FileLogAppender>(config_appender.file_path, config_appender.level);
+                default:
+                    std::cerr << "__LoggerManager::init exception invalid appender' values, appender.type=" << config_appender.output_type << std::endl;
+                    break;
+            }
+            //如果定义了appender的日志格式，个性化formatter，否则跟随logger
+            if(!config_appender.formatter.empty()) {
+                appender->setFormatter(std::make_shared<LogFormatter>(config_appender.formatter));
+            }
+            logger->addAppender(std::move(appender));
+        }
+        std::cout << "create Logger successfully " << config_log.name << std::endl;
+        m_logger_map.insert(std::make_pair(config_log.name, std::move(logger)));
+    }
+    ensureGlobalLoggerExists();
+}
+
+template<class T>
+void __LoggerManager::update_init(const T& config_log_list) 
+{
+    std::cout << "update_init" << config_log_list.size() << std::endl;
+    for(const auto& config_log : config_log_list) {
+        m_logger_map.erase(config_log.name);    //删除同名的logger
+        auto logger = std::make_shared<Logger>(config_log.name,config_log.level,config_log.formatter);
+
+        for(const auto& config_appender : config_log.appender) {
+            LogAppender::_ptr appender;
+            switch (config_appender.output_type)
+            {
+                case LogAppenderConfig::STDOUT:
+                    appender = std::make_shared<StdoutLogAppender>(config_appender.level);
+                    break;
+                case LogAppenderConfig::FILE:
+                    appender = std::make_shared<FileLogAppender>(config_appender.file_path, config_appender.level);
+                default:
+                    std::cerr << "__LoggerManager::init exception invalid appender' values, appender.type=" << config_appender.output_type << std::endl;
+                    break;
+            }
+            //如果定义了appender的日志格式，个性化formatter，否则跟随logger
+            if(!config_appender.formatter.empty()) {
+                appender->setFormatter(std::make_shared<LogFormatter>(config_appender.formatter));
+            }
+            logger->addAppender(std::move(appender));
+        }
+        std::cout << "create Logger successfully " << config_log.name << std::endl;
+        m_logger_map.insert(std::make_pair(config_log.name, std::move(logger)));
+    }
+}
+
+void __LoggerManager::ensureGlobalLoggerExists()
+{
+    auto iter = m_logger_map.find("global");
+    if(iter == m_logger_map.end()){
+        auto global_logger = std::make_shared<Logger>();
+        global_logger->addAppender(std::make_shared<StdoutLogAppender>());
+        m_logger_map.insert(std::make_pair("global", std::move(global_logger)));
+    }
+    else if(!iter->second){
+        iter->second = std::make_shared<Logger>();
+        iter->second->addAppender(std::make_shared<StdoutLogAppender>());
+    }
+}
+
+Logger::_ptr __LoggerManager::getLogger(const std::string& name)
+{
+    ScopedLock lock(&m_mutex);
+    auto iter = m_logger_map.find(name);
+    if(iter == m_logger_map.end()){
+        return m_logger_map["global"];
+    }
+    else{
+        return iter->second;
+    }
+}
+
+Logger::_ptr __LoggerManager::getGlobal()
+{
+    return m_logger_map["global"];
+}
+}
+
+
+
---
+++ code/log/log.h
@@ -0,0 +1,370 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <fstream>
+#include <iostream>
+
+#include "singleInstance.h"
+#include "lexicalcast.h"
+#include "lock.h"
+#include "config.h"
+#include "util.h"
+
+#define MAKE_LOG_EVENT(level, message)  \
+    std::make_shared<hxk::LogEvent>(__FILE__, __LINE__, hxk::GetThreadID(),hxk::GetFiberID(), ::time(nullptr), message, level)
+
+#define LOG_LEVEL(logger, level, message) \
+    logger->log(MAKE_LOG_EVENT(level, message));
+
+#define LOG_DEBUG(logger, message) LOG_LEVEL(logger, hxk::LogLevel::DEBUG, message)
+#define LOG_INFO(logger, message) LOG_LEVEL(logger, hxk::LogLevel::INFO, message)
+#define LOG_WARN(logger, message) LOG_LEVEL(logger, hxk::LogLevel::WARN, message)
+#define LOG_ERROR(logger, message) LOG_LEVEL(logger, hxk::LogLevel::ERROR, message)
+#define LOG_FATAL(logger, message) LOG_LEVEL(logger, hxk::LogLevel::FATAL, message)
+
+#define LOG_FORMAT_LEVEL(logger, level, format, argv...)        \
+    {                                                           \
+        char* _buf = nullptr;                                   \
+        int len = asprintf(&_buf, format, argv);                \
+        if(len != -1){                                          \
+            LOG_LEVEL(logger, level, std::string(_buf, len));   \
+            free(_buf);                                         \
+        }                                                       \
+    }
+
+#define LOG_FORMAT_DEBUG(logger, format, argv...) LOG_FORMAT_LEVEL(logger, hxk::LogLevel::DEBUG, format, argv)
+#define LOG_FORMAT_INFO(logger, format, argv...) LOG_FORMAT_LEVEL(logger, hxk::LogLevel::INFO, format, argv)
+#define LOG_FORMAT_WARN(logger, format, argv...) LOG_FORMAT_LEVEL(logger, hxk::LogLevel::WARN, format, argv)
+#define LOG_FORMAT_ERROR(logger, format, argv...) LOG_FORMAT_LEVEL(logger, hxk::LogLevel::ERROR, format, argv)
+#define LOG_FORMAT_FATAL(logger, format, argv...) LOG_FORMAT_LEVEL(logger, hxk::LogLevel::FATAL, format, argv)
+
+#define GET_ROOT_LOGGER() hxk::LoggerManager_Ptr::GetInstance()->getGlobal()
+#define GET_LOGGER(name) hxk::LoggerManager_Ptr::GetInstance()->getLogger(name)
+
+namespace hxk
+{
+
+/// @brief 日志级别
+class LogLevel
+{
+public:
+    enum Level{
+        UNKNOWN = 0,
+        DEBUG,
+        INFO,
+        WARN,
+        ERROR,
+        FATAL
+    };
+
+    static std::string levelToString(LogLevel::Level level);
+};
+
+
+/// @brief 日志器的Appender的配置信息类
+struct LogAppenderConfig
+{
+    enum Type
+    {
+        STDOUT = 0,
+        FILE
+    };
+    LogAppenderConfig::Type output_type; //输出器的类型
+    LogLevel::Level level;  //输出器的日志有效等级
+    std::string formatter;  //输出器的日志打印格式
+    std::string file_path;  //输出器的目标文件路径
+
+    LogAppenderConfig():output_type(Type::STDOUT), level(LogLevel::UNKNOWN){}
+
+    bool operator==(const LogAppenderConfig& lhs) const
+    {
+        return output_type == lhs.output_type &&
+                level == lhs.level &&
+                formatter == lhs.formatter &&
+                file_path == lhs.file_path;
+    }
+
+};
+
+
+
+/// @brief 日志器的配置信息类
+struct LogConfig
+{
+    std::string name;   //日志器的名称
+    LogLevel::Level level;
+    std::string formatter;
+    std::vector<LogAppenderConfig> appender;    //日志器的输出器配置集合
+
+    LogConfig():level(LogLevel::UNKNOWN){}
+
+    bool operator==(const LogConfig& lhs) const
+    {
+        return name == lhs.name;
+    }
+};
+
+/**
+ * @Author: hxk
+ * @brief: 日志消息类
+ * @return {*}
+ */
+class LogEvent
+{
+public:
+    typedef std::shared_ptr<LogEvent> _ptr;
+    
+    LogEvent(const std::string& filename, uint32_t line, uint32_t threadid, uint32_t fiberid, time_t time, const std::string& content, LogLevel::Level level = LogLevel::DEBUG);
+
+    LogLevel::Level getLevel() const ;
+    void setLevel(LogLevel::Level level) ;
+
+    const std::string& getFilename() const;
+    uint32_t getLine() const ;
+    uint32_t getThreadId() const ;
+    uint32_t getFiberId() const ;
+    time_t getTime() const ;
+    const std::string& getContent() const ;
+
+private:
+    LogLevel::Level m_level;    //日志等级
+    std::string m_filename;     //文件名
+    uint32_t m_line;            //行号
+    uint32_t m_thread_id;       //线程id
+    uint32_t m_fiber_id;        //携程id
+
+    time_t m_time;              //时间
+    std::string m_content;      //内容
+};
+
+class FormatItem
+{
+public:
+    typedef std::shared_ptr<FormatItem> _ptr;
+    virtual void format(std::ostream& out, LogEvent::_ptr ev) = 0;
+};
+
+/**
+ * @Author: hxk
+ * @brief: 日志格式化器
+ *  构造时传入日志格式化字符串，调用format()传入LogEvent实例，返回格式化后的字符串
+ * @return {*}
+ */
+class LogFormatter
+{
+public:
+    typedef std::shared_ptr<LogFormatter> _ptr;
+
+    explicit LogFormatter(const std::string& pattern);
+    std::string format(LogEvent::_ptr ev); 
+
+private:
+    void init();
+
+    std::string m_format_pattern;       
+    std::vector<FormatItem::_ptr> m_formatItem_list;
+};
+
+/**
+ * @Author: hxk
+ * @brief: 日志输出器基类
+ * @return {*}
+ */
+class LogAppender
+{
+public:
+    typedef std::shared_ptr<LogAppender> _ptr;
+
+    explicit LogAppender(LogLevel::Level level = LogLevel::DEBUG);
+    virtual ~LogAppender() = default;
+
+    virtual void log(LogLevel::Level level, LogEvent::_ptr ev) = 0;
+
+    LogFormatter::_ptr getFormatter();  //thread-safe获取格式化器
+
+    void setFormatter(LogFormatter::_ptr formatter);    //thread-safe设置格式化器
+
+protected:
+    LogLevel::Level m_level;
+    LogFormatter::_ptr m_formatter;
+    Mutex m_mutex;
+};
+
+/**
+ * @Author: hxk
+ * @brief: 输出到终端的Appender
+ * @return {*}
+ */
+class StdoutLogAppender : public LogAppender
+{
+public:
+    typedef std::shared_ptr<StdoutLogAppender> _ptr;
+
+    explicit StdoutLogAppender(LogLevel::Level level = LogLevel::DEBUG);
+
+    void log(LogLevel::Level level, LogEvent::_ptr ev) override;
+};
+
+/**
+ * @Author: hxk
+ * @brief: 输出到文件的Appender
+ * @return {*}
+ */
+class FileLogAppender : public LogAppender
+{
+public:
+    typedef std::shared_ptr<FileLogAppender> _ptr;
+
+    explicit FileLogAppender(const std::string& filename, LogLevel::Level level = LogLevel::DEBUG);
+
+    void log(LogLevel::Level level, LogEvent::_ptr ev) override;
+
+    bool reopen();
+
+private:
+    std::string m_filename;
+    std::ofstream m_file_stream;
+};
+
+class Logger
+{
+public:
+    typedef std::shared_ptr<Logger> _ptr;
+
+    Logger();
+    Logger(const std::string& name, LogLevel::Level level, const std::string& pattern);
+
+    void log(LogEvent::_ptr ev);
+
+    void addAppender(LogAppender::_ptr appender);
+    void delAppender(LogAppender::_ptr appender);
+
+    LogLevel::Level getLevel() const ;
+    void setLevel(LogLevel::Level level) ;
+
+    std::string getName() const ;
+private:
+    LogLevel::Level m_level;
+    const std::string m_name;   //日志器名称
+    std::string m_formatter_pattern;    //日志输出格式化器的默认pattern
+    LogFormatter::_ptr m_formatter;     //日志默认格式化器
+    std::list<LogAppender::_ptr> m_appender_list;
+    Mutex m_mutex;
+};
+
+class __LoggerManager
+{
+public:
+    typedef std::shared_ptr<__LoggerManager> _ptr;
+    __LoggerManager();
+
+    Logger::_ptr getLogger(const std::string& name);    //传入日志器名称来获取日志器
+    Logger::_ptr getGlobal();   //返回全局日志器
+
+private:
+    friend struct LogIniter;
+
+    void init(std::vector<LogConfig> config_log_vec = {});
+    void ensureGlobalLoggerExists();
+
+    template<class T>
+    void update_init(const T& val);
+
+    std::map<std::string, Logger::_ptr> m_logger_map;
+    Mutex m_mutex;
+};
+
+/**
+ * @Author: hxk
+ * @brief: __LoggerManager的单例类
+ * @return {*}
+ */
+typedef SingleInstancePtr<__LoggerManager> LoggerManager_Ptr;
+
+struct LogIniter
+{
+    LogIniter()
+    {
+        auto log_config_list = hxk::Config::lookUp<std::vector<LogConfig>>("logs", {}, "日志器的配置项");
+        log_config_list->addListener([](const std::vector<LogConfig>& old_val, const std::vector<LogConfig>& new_val){
+            std::cout << "日志器配置变动，更新日志器" << std::endl;
+            LoggerManager_Ptr::GetInstance()->init(new_val);
+            //LoggerManager_Ptr::GetInstance()->update_init<std::vector<LogConfig>>(new_val);
+        });
+    }
+};
+
+static LogIniter __log_initer__;    //静态变量，程序开始一直存在
+}
+
+namespace hxk
+{
+template <>
+class LexicalCast<std::string, std::vector<LogConfig>>
+{
+public:
+    std::vector<LogConfig> operator()(const std::string& source)
+    {
+        auto node = YAML::Load(source);
+        std::vector<LogConfig> res;
+        if(node.IsSequence()){
+            for(const auto& log_config : node){
+                LogConfig log_cfg;
+                log_cfg.name = log_config["name"] ? log_config["name"].as<std::string>():"";
+                log_cfg.level = log_config["level"] ? (LogLevel::Level)(log_config["level"].as<int>()) : LogLevel::UNKNOWN;
+                log_cfg.formatter = log_config["formatter"] ? log_config["formatter"].as<std::string>(): "";
+
+                if(log_config["appender"] && log_config["appender"].IsSequence()){
+                    for(const auto& app_config : log_config["appender"]){
+                        LogAppenderConfig appender_cfg{};
+                        appender_cfg.output_type = (LogAppenderConfig::Type)(app_config["type"] ? app_config["type"].as<int>() : 0);
+                        appender_cfg.file_path = app_config["file"] ? app_config["file"].as<std::string>() : "";
+                        appender_cfg.level = (LogLevel::Level)(app_config["level"] ? app_config["level"].as<int>() : log_cfg.level);
+                        appender_cfg.formatter = app_config["formatter"] ? app_config["formatter"].as<std::string>() : log_cfg.formatter;
+                        log_cfg.appender.push_back(appender_cfg);
+                    }
+                }
+
+                res.push_back(log_cfg);
+            }
+        }
+        std::cout << "lex: " << res.size() << std::endl; 
+        return res;
+    }
+};
+
+
+template<>
+class LexicalCast<std::vector<LogConfig>, std::string>
+{
+public:
+    std::string operator()(const std::vector<LogConfig>& source)
+    {
+        YAML::Node res;
+        for(const auto& log_config : source){
+            YAML::Node node;
+            node["name"] = log_config.name;
+            node["level"] = (int)log_config.level;
+            node["formatter"] = log_config.formatter;
+
+            YAML::Node app_node_list;
+            for(const auto& app_config : log_config.appender){
+                YAML::Node app_node;
+                app_node["type"] = (int)app_config.output_type;
+                app_node["file"] = app_config.file_path;
+                app_node["level"] = (int)app_config.level;
+                app_node["formatter"] = app_config.formatter;
+                app_node_list.push_back(app_node);
+            }
+
+            node["appender"] = app_node_list;
+            res.push_back(node);
+        }
+
+        std::stringstream ss;
+        ss << res;
+        return ss.str();
+    }
+};
+}
