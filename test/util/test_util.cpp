993bc9dcc09d789053c32edead85cb99ff37bd30
Your Name <you@example.com>
2023-07-10 12:46:51 +0800

    test util class

---
+++ test/util/test_util.cpp
@@ -0,0 +1,17 @@
+#include "util.h"
+#include <iostream>
+
+int main()
+{
+    uint64_t tmp =0;
+    uint64_t begin = hxk::GetCurrentMS();
+
+    for(int i=0;i<655360;i++){
+        tmp+=hxk::GetCurrentMS();
+        tmp%=655360;
+    }
+
+    uint64_t end = hxk::GetCurrentMS();
+    std::cout << end - begin <<std::endl;
+    return 0;
+}
