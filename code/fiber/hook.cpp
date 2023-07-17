70514a079f69589f922c798ea019fe483a8b4950
Helens-dot <374961015@qq.com>
2023-07-17 21:53:02 +0800

    add hook class

--- code/fiber/hook.cpp
+++ code/fiber/hook.cpp
@@ -1,8 +1,15 @@
 #include "hook.h"
 
+#include "log.h"
+#include "config.h"
+
 namespace hxk
 {
+static Logger::_ptr g_logger  = GET_LOGGER("system");
+
 static thread_local bool t_hook_enabled = false;
+
+static hxk::ConfigVar<int>::_ptr g_tcp_connect_timeout = hxk::Config::lookUp("tcp.connect.timeout", 5000);
 
 
 bool isHookEnabled()
@@ -13,6 +20,71 @@ bool isHookEnabled()
 void setHookEnable(bool flag)
 {
     t_hook_enabled = flag;
+}
+
+#define DEAL_FUNC(DO) \
+    DO(sleep)           \
+    DO(usleep) \
+    DO(nanosleep) \
+    DO(socket) \
+    DO(connect) \
+    DO(accept) \
+    DO(recv) \
+    DO(recvfrom) \
+    DO(recvmsg) \
+    DO(send) \
+    DO(sendto) \
+    DO(sendmsg) \
+    DO(getsockopt) \
+    DO(setsockopt) \
+    DO(read) \
+    DO(write) \
+    DO(close) \
+    DO(readv) \
+    DO(writev) \
+    DO(fcntl) \
+    DO(ioctl)
+
+void hook_init()
+{
+    static bool is_inited = false;
+    if(is_inited) {
+        return;
+    }
+
+    #define TRY_LOAD_HOOK_FUNC(name) name##_f = (name##_func)dlsym(RTLD_NEXT, #name);
+        DEAL_FUNC(TRY_LOAD_HOOK_FUNC)
+    #undef TRY_LOAD_HOOK_FUNC
+}
+
+static uint64_t s_connect_timeout = -1;
+struct _HookIniter
+{
+    _HookIniter()
+    {
+        hook_init();
+        s_connect_timeout = g_tcp_connect_timeout->getValue();
+        g_tcp_connect_timeout->addListener([](const int& old_value, const int& new_val){
+            LOG_FORMAT_INFO(g_logger, "tcp connect timeout change from %d to %d", old_value, new_val);
+            s_connect_timeout = new_val;
+        });
+    }
+};
+
+static _HookIniter s_hook_initer;
+
+
+struct TimerInfo
+{
+    int cancelled  = 0;
+};
+
+template<typename OriginFunc, typename ...Args>
+static ssize_t doIO(int fd, OriginFunc func, const char* hook_func_name, uint32_t event, int fd_timeout_type, Args&& ...args)
+{
+    if(!isHookEnabled()){
+        return func(fd, std::forward<Args>(args)...);
+    }
 }
 
 } // namespace hxk
--- code/fiber/hook.h
+++ code/fiber/hook.h
@@ -1,9 +1,93 @@
 #pragma once
 
+#include <time.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <sys/ioctl.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <dlfcn.h>
+
 namespace hxk
 {
 bool isHookEnabled();
 
 void setHookEnable(bool flag);
+
+}
+
+
+extern "C"
+{
+
+/// @brief time.h
+typedef unsigned int (*sleep_func)(unsigned int seconds);
+extern sleep_func sleep_f;
+
+typedef int (*usleep_func)(useconds_t usec);
+extern usleep_func usleep_f;
+
+typedef int (*nanosleep_func)(const struct timespec* req, struct timespec* rem);
+extern nanosleep_func nanosleep_f;
+
+/// @brief sys/socket.h
+typedef int (*socket_func)(int domain, int type, int protocol);
+extern socket_func socket_f;
+
+typedef int (*connect_func)(int sock_fd, const struct sockaddr* addr, socklen_t addrlen);
+extern connect_func connect_f;
+
+typedef int (*accept_func)(int sock_fd, struct sockaddr* addr, socklen_t* addrlen);
+extern accept_func accept_f;
+
+typedef ssize_t (*recv_func)(int sock_fd, void* buf, size_t len, int flags);
+extern recv_func recv_f;
+
+typedef ssize_t (*recvfrom_func)(int sock_fd, void* buf, size_t len, int flags, struct sockaddr* addr, socklen_t* addrlen);
+extern recvfrom_func recvfrom_f;
+
+typedef ssize_t (*recvmsg_func)(int sock_fd, struct msghdr* msg, int flags);
+extern recvmsg_func recvmsg_f;
+
+typedef ssize_t (*send_func)(int sock_fd, const void* buf, size_t len, int flags);
+extern send_func send_f;
+
+typedef ssize_t (*sendto_func)(int sock_fd, const void* buf, size_t len, int flags, const struct sockaddr* dest_addr, socklen_t addr_len);
+extern sendto_func sendto_f;
+
+typedef ssize_t (*sendmsg_func)(int sock_fd, const struct msghdr* msg, int flags);
+extern sendmsg_func sendmsg_f;
+
+typedef int (*getsockopt_func)(int sock_fd, int level, int optname, void* optval, socklen_t* optlen);
+extern getsockopt_func getsockopt_f;
+
+typedef int (*setsockopt_func)(int sock_fd, int level, int optname, const void* optval, socklen_t optlen);
+extern setsockopt_func setsockopt_f;
+
+/// @brief unistd.h
+typedef ssize_t (*read_func)(int fd, void* buf, size_t len);
+extern read_func read_f;
+
+typedef ssize_t (*write_func)(int fd, const void* buf, size_t count);
+extern write_func write_f;
+
+typedef int (*close_func)(int fd);
+extern close_func close_f;
+
+/// @brief sys/uio.h
+typedef ssize_t (*readv_func)(int fd, const struct iovec* iov, int iovcnt);
+extern readv_func readv_f;
+
+typedef ssize_t (*writev_func)(int fd, const struct iovec* iov, int iovcnt);
+extern writev_func writev_f;
+
+/// @brief fcntl.h
+typedef int (*fcntl_func)(int fd, int cmd, ...);
+extern fcntl_func fcntl_f;
+
+/// @brief sys/ioctl.h
+typedef int (*ioctl_func)(int fd, unsigned long request, ...);
+extern ioctl_func ioctl_f;
 
 }
